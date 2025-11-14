8e6562a9c106e508088cee259ddff594cb7fcdf6
ha-ayoub <hafdi.ayoub@gmail.com>
2025-11-14 14:09:41 +0100

    Configure vercel allow cors

--- ChatService/Program.cs
+++ ChatService/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "http://localhost:5173",
-            "http://localhost:3000")
+            "http://localhost:3000",
+            "https://chat-app-real-time-sable.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
